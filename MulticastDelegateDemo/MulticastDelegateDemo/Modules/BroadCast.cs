644f1158606a0a08cb1800d50e93417f42b111eb
LL <NZLILI2015@gmail.com>
2016-10-30 20:09:02 +1300

    Upload the demo code

---
+++ MulticastDelegateDemo/MulticastDelegateDemo/Modules/BroadCast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegateDemo.Modules
+{
+    class BroadCast
+    {
+        public void ViaMessage()
+        {
+            Console.WriteLine("Message sent to mobile.");
+        }
+
+        public void ViaEmail()
+        {
+            Console.WriteLine("Email sent to mailbox.");
+        }
+
+        public void ViaFax()
+        {
+            Console.WriteLine("Message sent via fax.");
+        }
+    }
+}
---
+++ MulticastDelegateDemo/MulticastDelegateDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using MulticastDelegateDemo.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegateDemo
+{
+    class Program
+    {
+        delegate void SendMessage();
+
+        static void Main(string[] args)
+        {
+            BroadCast broadCast = new BroadCast();
+
+            SendMessage sendMessage = broadCast.ViaEmail;
+            sendMessage += broadCast.ViaFax;
+            sendMessage += broadCast.ViaMessage;
+
+            Console.WriteLine("Enter anything: ");
+            var result = Console.ReadLine();
+            if (result.Equals("Pass"))
+            {
+                Console.WriteLine("Trasaction successful!");
+            }
+            else
+            {
+                Console.WriteLine("Transaction failed!");
+                sendMessage.Invoke();
+            }
+            Console.ReadLine();
+        }
+    }
+}
